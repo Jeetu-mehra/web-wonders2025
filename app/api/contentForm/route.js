342f9898cdfcef5cc98a9a04d6f87b7faa94b37b
ParamKanada <u24ai047@aid.svnit.ac.in>
2025-07-23 15:59:12 +0530

    successful database connection, tested with postman and compass

--- app/api/contentForm/route.js
+++ app/api/contentForm/route.js
@@ -6,6 +6,15 @@ export async function POST(request) {
     try {
         await connectionToDatabase();
         const { title, description, tags, imageURL, category, featureContent } = await request.json();
+        await Content.create({
+            title: title,
+            description: description,
+            tags: tags,
+            category: category,
+            featurContent: featureContent,
+            imageURL:imageURL
+        })
+        return NextResponse.json({message:"Content created"})
     }
     catch (err) {
         console.log(err);
