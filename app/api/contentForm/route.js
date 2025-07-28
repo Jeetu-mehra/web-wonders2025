5700b02208385a8613108470199a04802308b1b8
ParamKanada <u24ai047@aid.svnit.ac.in>
2025-07-28 18:56:09 +0530

    added route for the content form

---
+++ app/api/contentForm/route.js
@@ -0,0 +1,28 @@
+import connectionToDatabase from "@/lib/dbConnect";
+import Content from "@/Schemas/Content";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+    try {
+        await connectionToDatabase();
+        const { title, description, tags, imageURL, category, featureContent } = await request.json();
+        await Content.create({
+            title: title,
+            description: description,
+            tags: tags,
+            category: category,
+            // featurContent: featureContent,
+            // imageURL:imageURL
+        })
+        return NextResponse.json({message:"Content created"})
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+export async function GET (){
+    await connectionToDatabase();
+    const allContent = await Content.find();
+    return NextResponse.json({allContent});
+}
